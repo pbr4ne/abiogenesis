ebd2ea1ed850a88402b21319452086470ae9beac
pbr4ne <pbr4ne@protonmail.com>
2025-03-16 14:52:34 -0700

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    Abiogenesis
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+#app {
+  width: 100%;
+  height: 100svh;
+  position: relative;
+  overflow: hidden;
+}
+</style>
---
+++ src/utilities/naiveUI.ts
@@ -0,0 +1,10 @@
+import { 
+    create,
+  } from 'naive-ui';
+  
+  
+  export const naive = create({
+    components: [
+    ],
+  });
+  
