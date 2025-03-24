01da4b1c0e3664212a73efe8d7582ba9214f8dc1
pbr4ne <pbr4ne@protonmail.com>
2025-03-23 17:59:45 -0700

    refactor: move blip config to file

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -8,37 +8,9 @@
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import { useBlips } from '../composables/useBlips';
 import { emitter } from '../utilities/emitter';
+import { defaultConfig } from '../configs/default';
 
-const config = {
-  redInitial: 256,
-  greenInitial: 256,
-  blueInitial: 256,
-
-  redGrowRate: 0.15,
-  redEatRate: 0.5,
-  redDieRate: 0.1,
-  redStarveRate: 0.05,
-  redGrowSelfRate: 0.5,
-  redGrowOtherRate: 0.5,
-
-  blueGrowRate: 0.5,
-  blueEatRate: 1.0,
-  blueDieRate: 0.1,
-  blueStarveRate: 0.5,
-  blueGrowSelfRate: 0.5,
-  blueGrowOtherRate: 0.5,
-  blueEatenSelfRate: 0.5,
-  blueEatenOtherRate: 0.5,
-
-  greenGrowRate: 0.85,
-  greenDieRate: 0.1,
-  greenGrowSelfRate: 0.5,
-  greenGrowOtherRate: 0.5,
-  greenEatenSelfRate: 0.5,
-  greenEatenOtherRate: 0.5,
-};
-
-const { cellSize, init, process, draw, calculateAverageRGB } = useBlips(config);
+const { cellSize, init, process, draw, calculateAverageRGB } = useBlips(defaultConfig);
 
 const canvasRef = ref<HTMLCanvasElement | null>(null);
 
