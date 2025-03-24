213200a58cb98cd494a1b6ead0ff1486233cd6b8
pbr4ne <pbr4ne@protonmail.com>
2025-03-23 17:39:24 -0700

    refactor: add types

--- src/composables/useBlips.ts
+++ src/composables/useBlips.ts
@@ -1,37 +1,4 @@
-export interface Blip {
-  r: number;
-  g: number;
-  b: number;
-}
-
-export interface BlipsConfig {
-  INITIAL_R: number;
-  INITIAL_G: number;
-  INITIAL_B: number;
-
-  GROW_R: number;
-  EAT_R: number;
-  DIE_R: number;
-  STARVE_R: number;
-  GROW_SELF_R: number;
-  GROW_OTHER_R: number;
-
-  GROW_B: number;
-  EAT_B: number;
-  DIE_B: number;
-  STARVE_B: number;
-  GROW_SELF_B: number;
-  GROW_OTHER_B: number;
-  EATEN_SELF_B: number;
-  EATEN_OTHER_B: number;
-
-  GROW_G: number;
-  DIE_G: number;
-  GROW_SELF_G: number;
-  GROW_OTHER_G: number;
-  EATEN_SELF_G: number;
-  EATEN_OTHER_G: number;
-}
+import { Blip, BlipsConfig } from '../utilities/types';
 
 export function useBlips(config: BlipsConfig) {
   const cellSize = 5;
@@ -52,9 +19,9 @@ export function useBlips(config: BlipsConfig) {
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         current[y][x] = {
-          r: Math.floor(Math.random() * config.INITIAL_R),
-          g: Math.floor(Math.random() * config.INITIAL_G),
-          b: Math.floor(Math.random() * config.INITIAL_B),
+          r: Math.floor(Math.random() * config.redInitial),
+          g: Math.floor(Math.random() * config.greenInitial),
+          b: Math.floor(Math.random() * config.blueInitial),
         };
       }
     }
@@ -101,36 +68,36 @@ export function useBlips(config: BlipsConfig) {
         const avgBlue = neighborCount > 0 ? totalBlue / neighborCount : 0;
 
         //process red
-        const growRedSelf = self.b * config.GROW_R * config.GROW_SELF_R;
-        const growRedOther = avgBlue * config.GROW_R * config.GROW_OTHER_R;
+        const growRedSelf = self.b * config.redGrowRate * config.redGrowSelfRate;
+        const growRedOther = avgBlue * config.redGrowRate * config.redGrowOtherRate;
         const starveRedSelf = starveValue(
           self.r,
           (totalBlue + self.b) / (neighborCount + 1)
-        ) * config.STARVE_R;
-        const removeRed = self.r * config.DIE_R + starveRedSelf;
+        ) * config.redStarveRate;
+        const removeRed = self.r * config.redDieRate + starveRedSelf;
         const addRed = growRedSelf + growRedOther;
         const newR = roundColour(self.r + addRed - removeRed);
 
         //process blue
-        const growBlueSelf = self.g * config.GROW_B * config.GROW_SELF_B;
-        const growBlueOther = avgGreen * config.GROW_B * config.GROW_OTHER_B;
-        const eatenBlueSelf = self.r * config.EAT_R * config.EATEN_SELF_B;
-        const eatenBlueOther = avgRed * config.EAT_R * config.EATEN_OTHER_B;
+        const growBlueSelf = self.g * config.blueGrowRate * config.blueGrowSelfRate;
+        const growBlueOther = avgGreen * config.blueGrowRate * config.blueGrowOtherRate;
+        const eatenBlueSelf = self.r * config.redEatRate * config.blueEatenSelfRate;
+        const eatenBlueOther = avgRed * config.redEatRate * config.blueEatenOtherRate;
         const starveBlueSelf = starveValue(
           self.b,
           (totalGreen + self.g) / (neighborCount + 1)
-        ) * config.STARVE_B;
+        ) * config.blueStarveRate;
         const removeBlue =
-          self.b * config.DIE_B + eatenBlueSelf + eatenBlueOther + starveBlueSelf;
+          self.b * config.blueDieRate + eatenBlueSelf + eatenBlueOther + starveBlueSelf;
         const addBlue = growBlueSelf + growBlueOther;
         const newB = roundColour(self.b + addBlue - removeBlue);
 
         //process green
-        const growGreenSelf = self.g * config.GROW_G * config.GROW_SELF_G;
-        const growGreenOther = avgGreen * config.GROW_G * config.GROW_OTHER_G;
-        const eatenGreenSelf = self.b * config.EAT_B * config.EATEN_SELF_G;
-        const eatenGreenOther = avgBlue * config.EAT_B * config.EATEN_OTHER_G;
-        const removeGreen = self.g * config.DIE_G + eatenGreenSelf + eatenGreenOther;
+        const growGreenSelf = self.g * config.greenGrowRate * config.greenGrowSelfRate;
+        const growGreenOther = avgGreen * config.greenGrowRate * config.greenGrowOtherRate;
+        const eatenGreenSelf = self.b * config.blueEatRate * config.greenEatenSelfRate;
+        const eatenGreenOther = avgBlue * config.blueEatRate * config.greenEatenOtherRate;
+        const removeGreen = self.g * config.greenDieRate + eatenGreenSelf + eatenGreenOther;
         const addGreen = growGreenSelf + growGreenOther;
         const newG = roundColour(self.g + addGreen - removeGreen);
 
---
+++ src/utilities/types.ts
@@ -0,0 +1,34 @@
+export interface Blip {
+  r: number;
+  g: number;
+  b: number;
+}
+
+export interface BlipsConfig {
+  redInitial: number;
+  greenInitial: number;
+  blueInitial: number;
+
+  redGrowRate: number;
+  redEatRate: number;
+  redDieRate: number;
+  redStarveRate: number;
+  redGrowSelfRate: number;
+  redGrowOtherRate: number;
+
+  blueGrowRate: number;
+  blueEatRate: number;
+  blueDieRate: number;
+  blueStarveRate: number;
+  blueGrowSelfRate: number;
+  blueGrowOtherRate: number;
+  blueEatenSelfRate: number;
+  blueEatenOtherRate: number;
+
+  greenGrowRate: number;
+  greenDieRate: number;
+  greenGrowSelfRate: number;
+  greenGrowOtherRate: number;
+  greenEatenSelfRate: number;
+  greenEatenOtherRate: number;
+}
