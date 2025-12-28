83c87671e39e488c299838ec82449910822332a6
pbr4ne <pbr4ne@protonmail.com>
2025-12-27 18:17:55 -0800

    feat: bigger planet

--- src/prefabs/Planet.ts
+++ src/prefabs/Planet.ts
@@ -1,14 +1,33 @@
 
-import { log } from "../utilities/GameUtils";
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
 
 export default class Planet extends Phaser.GameObjects.Container {
 
-	constructor(scene: Phaser.Scene) {
-		super(scene);
+	constructor(scene: Phaser.Scene, x?: number, y?: number) {
+		super(scene, x ?? 0, y ?? 0);
 
-		log("Planet constructor");
+		// planet
+		const planet = scene.add.ellipse(959, 499, 768, 768);
+		planet.isFilled = true;
+		this.add(planet);
 
-		const ellipse = new Phaser.GameObjects.Ellipse(scene, 0, 0, 512, 512, 0xffffff);
-		this.add(ellipse);
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
 	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
 }
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
