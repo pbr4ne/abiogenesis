4adcfd170fc75cfbb452afaadb550acc272b1036
pbr4ne <pbr4ne@protonmail.com>
2026-01-03 18:59:52 -0800

    feat: mouse over information

--- src/phases/evolution/Evolution.ts
+++ src/phases/evolution/Evolution.ts
@@ -1,6 +1,7 @@
 import { log } from "../../utilities/GameUtils";
 import PhaseScene from "../../scenes/PhaseScene";
 import Planet from "./EvolutionPlanet";
+import { LifeFormDef, LifeFormInstance } from "./EvolutionTypes";
 
 export default class Evolution extends PhaseScene {
   private planet!: Planet;
@@ -14,6 +15,68 @@ export default class Evolution extends PhaseScene {
     this.add.existing(this.planet);
     this.bgCam.ignore(this.planet);
 
-    log("Evolution scene created");
+    this.createLifeInfoPanel();
+    this.listenForLifeHover();
+  }
+
+  private infoBg!: Phaser.GameObjects.Rectangle;
+  private infoText!: Phaser.GameObjects.Text;
+
+  private listenForLifeHover() {
+    this.events.on("life:hover", (payload: { lf: LifeFormInstance; def: LifeFormDef } | null) => {
+      if (!payload) {
+        this.infoBg.setVisible(false);
+        this.infoText.setVisible(false);
+        return;
+      }
+
+      const { lf, def } = payload;
+
+      const habitats = def.habitats.join(", ");
+      const mutates = def.mutatesTo.length ? def.mutatesTo.join(", ") : "none";
+
+      this.infoText.setText(
+        [
+          `${def.type.toUpperCase()}`,
+          ``,
+          `Habitat: ${habitats}`,
+          `Rarity: ${def.rarity}`,
+          `Mutates to: ${mutates}`,
+          ``,
+          `Mutation: ${lf.mutationRate}/10`,
+          `Reproduction: ${lf.reproductionRate}/10`,
+          `Survival: ${lf.survivalRate}/10`,
+          ``,
+          `Cell: (${lf.row}, ${lf.col})`
+        ].join("\n")
+      );
+
+      this.infoBg.setVisible(true);
+      this.infoText.setVisible(true);
+    });
+  }
+
+  private createLifeInfoPanel() {
+    const w = 360;
+    const h = 260;
+    const pad = 16;
+
+    const x = this.scale.width - w / 2 - 24;
+    const y = 180;
+
+    this.infoBg = this.add.rectangle(x, y, w, h, 0x0b0b0b, 0.75);
+    this.infoBg.setStrokeStyle(2, 0xffffff, 0.2);
+    this.infoBg.setScrollFactor(0);
+
+    this.infoText = this.add.text(x - w / 2 + pad, y - h / 2 + pad, "", {
+      fontFamily: "Arial",
+      fontSize: "18px",
+      color: "#ffffff",
+      wordWrap: { width: w - pad * 2 }
+    });
+    this.infoText.setScrollFactor(0);
+
+    this.infoBg.setVisible(false);
+    this.infoText.setVisible(false);
   }
 }
--- src/phases/evolution/EvolutionPlanet.ts
+++ src/phases/evolution/EvolutionPlanet.ts
@@ -4,12 +4,17 @@ import { paintHydrosphere } from "../terraform/HydrosphereMap";
 import { ensureStartingProkaryotes } from "./EvolutionSpawn";
 import { LIFEFORMS } from "./LifeForms";
 import { drawCellBump } from "../../planet/PlanetRenderer";
+import { LifeFormInstance } from "./EvolutionTypes";
+import { pickCellByNearestProjectedCenter } from "../../planet/PlanetMath";
 
 const rgbToHexStr = (r: number, g: number, b: number) =>
   "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
 
 export default class EvolutionPlanet extends PlanetBase {
   private run: PlanetRunState;
+
+  private lifeByCell = new Map<string, LifeFormInstance>();
+  private hoveredLifeId: string | null = null;
 
   constructor(scene: Phaser.Scene, x = 960, y = 540) {
     super(scene, x, y);
@@ -26,6 +31,68 @@ export default class EvolutionPlanet extends PlanetBase {
     this.renderLifeForms();
     this.renderLifeBumps();
     this.redrawTiles();
+
+    this.rebuildLifeIndex();
+    this.enableLifeHover();
+  }
+
+  private keyOf(row: number, col: number) {
+    return `${row},${col}`;
+  }
+
+  private rebuildLifeIndex() {
+    this.lifeByCell.clear();
+    for (const lf of this.run.lifeForms) {
+      this.lifeByCell.set(this.keyOf(lf.row, lf.col), lf);
+    }
+  }
+
+  public enableLifeHover() {
+    this.onPlanetPointerMove(pointer => {
+      const dx = pointer.worldX - this.x;
+      const dy = pointer.worldY - this.y;
+
+      const cell = pickCellByNearestProjectedCenter(dx, dy, this.r, this.divisions, this.rotate);
+      if (!cell) {
+        this.setCursorDefault();
+        this.emitHover(null);
+        return;
+      }
+
+      const lf = this.lifeByCell.get(this.keyOf(cell.row, cell.col)) ?? null;
+
+      if (!lf) {
+        this.setCursorDefault();
+        this.emitHover(null);
+        return;
+      }
+
+      this.setCursorPointer();
+
+      if (this.hoveredLifeId !== lf.id) {
+        this.hoveredLifeId = lf.id;
+        this.emitHover(lf);
+      }
+    });
+  }
+
+  private emitHover(lf: LifeFormInstance | null) {
+    if (!lf) {
+      this.hoveredLifeId = null;
+      this.scene.events.emit("life:hover", null);
+      return;
+    }
+
+    const def = LIFEFORMS[lf.type];
+    this.scene.events.emit("life:hover", { lf, def });
+  }
+
+  private setCursorPointer() {
+    this.scene.input.setDefaultCursor("pointer");
+  }
+
+  private setCursorDefault() {
+    this.scene.input.setDefaultCursor("default");
   }
 
   private renderLifeForms() {
